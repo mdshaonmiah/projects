5a39b208986b0e0ad23efa7fd32852c034254d57
mdshaonmiah <shaon.miah1247@gmail.com>
2023-01-07 03:12:27 +0600

    first commit

---
+++ resources/js/main.js
@@ -0,0 +1,11 @@
+// mix it up
+var mixer = mixitup('.work-grid');
+//wow
+new WOW().init();
+//MENU
+function openMenu(){
+     document.getElementById('menu-container').style.width = '100%';
+}
+function closeMenu(){
+    document.getElementById('menu-container').style.width = '0%';
+}
